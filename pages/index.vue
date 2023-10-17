dd45754e4b26b0131efbeaaa91a4714ebdd8cdf5
Daigo Adachi <140701695+hourensou26@users.noreply.github.com>
2023-10-17 05:29:58 +0000

    ab

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,6 @@
   <li><nuxt-link to="/create">新規作成</nuxt-link></li>
   <li><nuxt-link to="/about">詳細</nuxt-link></li>
   <li><nuxt-link to="/grid">グリッド</nuxt-link></li>
+  <li><nuxt-link to="/recipe">リスト</nuxt-link></li>
 </ul>
-
-
 </template>
