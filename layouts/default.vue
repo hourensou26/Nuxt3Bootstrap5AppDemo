e086907eb37a678ab60f6470b5fd3c74b2d5805c
Daigo Adachi <140701695+hourensou26@users.noreply.github.com>
2023-10-17 07:42:43 +0000

    aff

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-  <nav>aaaaa</nav>
-    <slot />
-  </div>
+  <nav class="navbar bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">ToDo</a>
+    </div>
+  </nav>
+  <slot />
+
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-<h1>予定アプリ</h1>
-<ul>
-  <li><nuxt-link to="/create">新規作成</nuxt-link></li>
-  <li><nuxt-link to="/about">詳細</nuxt-link></li>
-  <li><nuxt-link to="/grid">グリッド</nuxt-link></li>
-  <li><nuxt-link to="/recipe">リスト</nuxt-link></li>
-</ul>
+  <h1 class="m-5">トップページ</h1>
+  
+  <div class="">
+    <nuxt-link class="btn-m-3 btn btn-primary" role="button" to="/create">新規作成</nuxt-link>
+    <nuxt-link class="btn-m-3 btn btn-primary" role="button" to="/about">詳細</nuxt-link>
+  </div>
+  
+  <div>
+    <h2 class="m-5" style="text-align: center;">予定アプリ</h2>
+  </div>
+
+
 </template>
