5ad4583d4e7654ee08efec165a236f6f91aa2b29
Daigo Adachi <140701695+hourensou26@users.noreply.github.com>
2023-10-23 06:30:36 +0000

    CREATE

--- pages/create.vue
+++ pages/create.vue
@@ -4,10 +4,10 @@
         <nuxt-link class="btn-m-3 btn btn-primary" role="button" to="/">トップへ</nuxt-link>
         <div class="container">
             <div class="row">
-                <div class="col-md-8">
+                <div class="col-md-9">
                 <input type="text" class="form-control" v-model="title" placeholder="タイトル">
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-3">
                 <input type="date" class="form-control" v-model="date" placeholder="日付">
                 </div>
                 <div class="col">
@@ -15,7 +15,7 @@
                 </div>
             </div>
             <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-            <button class="btn btn-success" @click="create">作成</button>
+            <button class="btn btn-success" @click="create">新規作成</button>
             <button class="btn btn-danger" @click="cancel">キャンセル</button>
             </div>
         </div>
