2617382c89339b0258119f20fba49eb3cce7ed97
Daigo Adachi <140701695+hourensou26@users.noreply.github.com>
2023-10-23 06:17:43 +0000

    create

--- pages/create.vue
+++ pages/create.vue
@@ -1,21 +1,23 @@
 <template>
-    <h1 class="m-5">新規作成</h1>
-    <nuxt-link class="btn-m-3 btn btn-primary" role="button" to="/">トップへ</nuxt-link>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-            <input type="text" class="form-control" v-model="title" placeholder="タイトル">
+    <div class="container-fluid">
+        <h1 class="m-5">新規作成</h1>
+        <nuxt-link class="btn-m-3 btn btn-primary" role="button" to="/">トップへ</nuxt-link>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                <input type="text" class="form-control" v-model="title" placeholder="タイトル">
+                </div>
+                <div class="col-md-4">
+                <input type="date" class="form-control" v-model="date" placeholder="日付">
+                </div>
+                <div class="col">
+                <textarea class="form-control" v-model="content" placeholder="詳細"></textarea>
+                </div>
             </div>
-            <div class="col-md-4">
-            <input type="date" class="form-control" v-model="date" placeholder="日付">
-            </div>
-            <div class="col">
-            <textarea class="form-control" v-model="content" placeholder="詳細"></textarea>
-            </div>
-        </div>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
             <button class="btn btn-success" @click="create">作成</button>
             <button class="btn btn-danger" @click="cancel">キャンセル</button>
+            </div>
         </div>
     </div>
 </template>
