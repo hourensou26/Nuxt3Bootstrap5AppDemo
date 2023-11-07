4a9c924c953383835d915adeec95848d5fadf6b7
Daigo Adachi <140701695+hourensou26@users.noreply.github.com>
2023-11-07 00:48:19 +0000

    kon

--- app.vue
+++ app.vue
@@ -5,3 +5,16 @@
       </NuxtLayout>
     </div>
     </template>
+
+
+import { createApp } from 'vue'
+
+const app = createApp({
+  data() {
+    return {
+      count: 0
+    }
+  }
+})
+
+app.mount('#app')
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
+<div id="app">
   <nav class="navbar bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">ToDo</a>
     </div>
   </nav>
+</div>
   <slot />
 
 </template>
