91ed99e22e95c166a4f3f5c72a91b1b086f13af5
Daigo Adachi <140701695+hourensou26@users.noreply.github.com>
2023-11-08 04:46:03 +0000

    kon

---
+++ components/hedder.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav class="navbar bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">ToDo</a>
+    </div>
+  </nav>
+  <slot />
+
+</template>
