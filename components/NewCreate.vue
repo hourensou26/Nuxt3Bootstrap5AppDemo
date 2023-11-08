9c57fca66e778338b31ef2fb7e7419af1bd38ddf
Daigo Adachi <140701695+hourensou26@users.noreply.github.com>
2023-11-08 04:48:23 +0000

    kon

---
+++ components/NewCreate.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      date: '',
+      content: ''
+    };
+  },
+  methods: {
+  create() {
+    const data = {
+      title: this.title,
+      date: this.date,
+      content: this.content
+      }
+      console.log('Create button clicked', data);
+      }
+    },
+    cancel() {
+      console.log('Cancel button clicked');
+    }
+}
+</script>
