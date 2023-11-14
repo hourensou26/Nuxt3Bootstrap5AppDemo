849cd6b2e0839eb050dfb17435c8e926881b9473
Daigo Adachi <140701695+hourensou26@users.noreply.github.com>
2023-11-14 07:12:45 +0000

    knon

---
+++ pages/test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-for="(item, index) in items" :key="index">
+      <!-- 要素に動的にIDを付与 -->
+      <p :id="'item-' + index">{{ item }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// TypeScriptの型定義
+interface MyItem {
+  id: number;
+  text: string;
+}
+
+// ダミーデータ
+const items: MyItem[] = [
+  { id: 1, text: 'アイテム1' },
+  { id: 2, text: 'アイテム2' },
+  { id: 3, text: 'アイテム3' },
+];
+
+// データやメソッドのロジック
+</script>
