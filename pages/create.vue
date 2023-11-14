53a3f7050a5423eb6f16b1349c5cfb7e5cbb0b16
Daigo Adachi <140701695+hourensou26@users.noreply.github.com>
2023-11-14 02:44:12 +0000

    kon

--- pages/create.vue
+++ pages/create.vue
@@ -1,3 +1,11 @@
+<script setup>
+import { ref } from 'vue';
+
+const title = ref('');
+const date = ref('');
+const content = ref('');
+</script>
+
 <template>
     <div class="container-fluid">
         <h1 class="m-5">新規作成</h1>
@@ -22,5 +30,32 @@
     </div>
 </template>
 
+<script>
+export default {
+
+  ListCreate() {
+    const create = () => {
+      // 新しいデータを作成
+      const newData = {
+        title: title.value,
+        date: date.value,
+        content: content.value,
+      };
+
+      // アクションをディスパッチ
+      createData(newData);
+    };
+
+    const cancel = () => {
+      // キャンセルの処理を追加
+    };
+
+    return { title, date, content, create, cancel };
+  },
+};
 
 
+store.commit(title,title.value)
+store.commit(date,date.value)
+store.commit(content,content.value)
+</script>
