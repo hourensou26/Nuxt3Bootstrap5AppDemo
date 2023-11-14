7949ce41ffcb860fddcb3168df162b210c769965
Daigo Adachi <140701695+hourensou26@users.noreply.github.com>
2023-11-14 05:02:15 +0000

    kon

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
 <div class="container-fluid">
 <h1 class="m-5">予定アプリ</h1>
 <nuxt-link class="btn-m-3 btn btn-primary" role="button" to="/create">新規作成</nuxt-link>
+
 </div>
   
   <div class="m-5" style="text-align: center;">
