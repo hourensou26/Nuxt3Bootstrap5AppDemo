d6d757a844093a7c15db62ae245399b992da56e7
Daigo Adachi <140701695+hourensou26@users.noreply.github.com>
2023-11-14 04:27:13 +0000

    kon

--- components/Navber.vue
+++ components/Navber.vue
@@ -1,17 +1,11 @@
 <template>
-<nav class="navbar navbar-expand-lg bg-dark">
+<nav class="navbar navbar-dark bg-dark">
   <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <NuxtLink class="navbar-brand text-light" href="/">Todo</NuxtLink>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-      <form class="d-flex ms-auto" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success text-light" type="submit">Search</button>
-      </form>
-    </div>
+    <a class="navbar-brand">Todo</a>
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
   </div>
 </nav>
-
 </template>
