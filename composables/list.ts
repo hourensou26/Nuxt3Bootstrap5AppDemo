488dfc8bad14efb7ee81732941317ba189a0b433
Daigo Adachi <140701695+hourensou26@users.noreply.github.com>
2023-11-15 05:44:16 +0000

    aa

--- composables/list.ts
+++ composables/list.ts
@@ -1,33 +1,17 @@
-// composables/dataHandler.ts
-
-import { ref, Ref } from 'vue';
+import { ref } from 'vue';
 
 interface SavedData {
   title: string;
-  date: string; // 日付を文字列として扱うこともあります
+  date: Date;
   content: string;
 }
 
-interface DataHandler {
-  inputData: Ref<SavedData>;
-  saveData: (data: SavedData) => void;
-  getInputData: () => SavedData;
+const inputData = ref<SavedData>({ title: '', date: new Date(), content: '' });
+
+export function saveData(data: SavedData): void {
+  inputData.value = { ...data };
 }
 
-const inputData = ref<SavedData>({ title: '', date: '', content: '' });
-
-export function useDataHandler(): DataHandler {
-  function saveData(data: SavedData): void {
-    inputData.value = { ...data };
-  }
-
-  function getInputData(): SavedData {
-    return { ...inputData.value };
-  }
-
-  return {
-    inputData,
-    saveData,
-    getInputData,
-  };
+export function getInputData(): SavedData {
+  return { ...inputData.value };
 }
