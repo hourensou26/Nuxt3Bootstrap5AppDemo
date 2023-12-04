44412e50a79f8be25ea2ea495d44c6b5af5aa614
Daigo Adachi <140701695+hourensou26@users.noreply.github.com>
2023-12-04 15:33:44 +0000

    onedit

--- components/Home.vue
+++ components/Home.vue
@@ -5,11 +5,13 @@
       <p class="card-text pt-2">期限：{{ item.date }}</p>
       <div class="d-flex justify-content-end flex-wrap">
         <nuxt-link class="btn btn-sm btn-secondary" role="button" @click="about(item)" to="/about">詳細</nuxt-link>
-        <nuxt-link class="btn btn-sm btn-success" @click="edit(item)" to="/detail">編集</nuxt-link>
+        <nuxt-link class="btn btn-sm btn-success" @click="onEdit(item.id)" to="/detail">編集</nuxt-link>
+        <button class="btn btn-sm btn-danger" @click="onEdit(item.id)">編集テスト</button>
         <button class="btn btn-sm btn-danger" @click="deleteItem(item)">削除</button>
       </div>
     </div>
   </div>
+  
 </template>
 
 <script setup>
@@ -20,10 +22,19 @@ onMounted(() => {
   items.value = JSON.parse(ls) || [];
 })
 
+let  = ref('');
+let  = ref('');
+let  = ref('');
+
 function about(item) {
   const index = items.value.findIndex((v) => v.id === item.id);
 
 }
+
+  function onEdit(id) {
+    a = items.value[id].content;
+    items.value[id].onEdit = true;
+  }
 
 function deleteItem(item) {
   if (!confirm('削除しますか？')) return;
--- pages/detail.vue
+++ pages/detail.vue
@@ -1,32 +1,9 @@
 <template>
   <div class="container-fluid">
-    <h1 class="m-5">{{ item.title }}</h1>
-    <div class="card w-50 mt-5" style="margin: auto;">
-      <div class="card-body">
-        <h4 class="card-title">タイトル：{{ index.title }}</h4>
-        <p class="card-text pt-2">期限：{{ index.date }}</p>
-        <div class="d-flex justify-content-end">
-          <nuxt-link class="btn-m-3 btn btn-primary" role="button" to="/">戻る</nuxt-link>
-        </div>
-      </div>
+
+    <h1 class="m-5">編集</h1>
+
+    <nuxt-link class="btn-m-3 btn btn-primary" role="button" to="/">トップへ</nuxt-link>
+    
     </div>
-  </div>
 </template>
-  
-<script setup>
-const item = ref({});
-
-const items = ref([])
-onMounted(() => {
-  const ls = localStorage.getItem('items');
-  items.value = JSON.parse(ls) || [];
-})
-
-
-onMounted(() => {
-  const ls = localStorage.getItem('items');
-  items.value = JSON.parse(ls) || [];
-  const id = this.$route.query.id;
-  item.value = items.value.find((v) => v.id === id);
-})
-</script>
