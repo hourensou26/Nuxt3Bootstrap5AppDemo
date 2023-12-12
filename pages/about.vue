a38380437b02bd9f4a74943793c71d757be4aa3e
Daigo Adachi <140701695+hourensou26@users.noreply.github.com>
2023-12-12 05:49:03 +0000

    kon

--- pages/about.vue
+++ pages/about.vue
@@ -6,9 +6,9 @@
 
        <div class="card w-50 mt-5" style="margin: auto;">
         <div class="card-body">
-            <h4 class="card-title">タイトル：{{ store.editTitle }}</h4>
-            <p class="card-text pt-2">期限：{{ store.editDate }}</p>
-            <p class="card-text pt-2">詳細：{{ store.editContent }}</p>
+            <h4 class="card-title">タイトル：{{ editTitle }}</h4>
+            <p class="card-text pt-2">期限：{{ editDate }}</p>
+            <p class="card-text pt-2">詳細：{{ editContent }}</p>
             <div class="d-flex justify-content-end flex-wrap">
                 <nuxt-link class="btn btn-sm btn-success" @click="edit(item)" to="/detail">編集</nuxt-link>
                 <button class="btn btn-sm btn-danger" @click="deleteItem(item)">削除</button>
@@ -18,8 +18,6 @@
 </template>
 
 <script setup>
-import { reactive } from 'vue';
-import { store } from '/workspaces/Nuxt3Bootstrap5AppDemo/components/Home.vue'
-
+import { store } from '~/composables/store.js'
 
 </script>
