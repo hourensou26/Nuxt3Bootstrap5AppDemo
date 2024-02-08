c4ca72e71932ccfd21d2876175a191f0131d7bc7
Daigo Adachi <140701695+hourensou26@users.noreply.github.com>
2024-02-08 03:36:55 +0000

    完成

--- components/Home.vue
+++ components/Home.vue
@@ -3,33 +3,25 @@
     <div class="card-body">
       <h4 class="card-title">タイトル：{{ item.title }}</h4>
       <p class="card-text pt-2">期限：{{ item.date }}</p>
-      <p class="card-text pt-2 fs-5">詳細：{{item.content }}</p>
+      <p v-if="item.showDetails" class="card-text pt-2 fs-5">詳細：{{ item.content }}</p>
       <div class="d-flex justify-content-end flex-wrap">
+        <button class="btn btn-sm btn-secondary" @click="about(item)">詳細</button>
         <button class="btn btn-sm btn-danger" @click="deleteItem(item)">削除</button>
       </div>
     </div>
   </div>
-
-  
 </template>
 
 <script setup>
 const items = ref([]);
-
 
 onMounted(() => {
   const ls = localStorage.getItem('items');
   items.value = JSON.parse(ls) || [];
 })
 
-function about(id) {
-  const pickedItem = items.value.find((v) => v.id === id);
-
-  editContent.value = pickedItem.content;
-  
-  return{
-    editContent: pickedItem.content,
-  }
+function about(item) {
+  item.showDetails = !item.showDetails;
 }
 
 function deleteItem(item) {
